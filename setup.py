0f1d5d11a88ac73907102869e17211de6c6e40dd
BubuDavid <david.pedroza.segoviano@gmail.com>
2021-10-09 14:15:16 -0500

    Add the setup version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
   name = 'bbfy',         # How you named your package folder (MyLib)
   packages = ['bbfy'],   # Chose the same as "name"
-  version = '0.1',      # Start with a small number and increase it with every change you make
+  version = '0.2',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'A simple API made with Python to consume Spotify API for my personal use',   # Give a short description about your library
   author = 'David Pedroza Segoviano (Bubu)',                   # Type in your name
